67f586c6cc40a08489fb5f83d2ab9da09a978bf1
jordyfriasdev <45669910+jordyfriasdev@users.noreply.github.com>
2021-07-09 00:02:54 -0400

    Initial commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json([
+
+            "data" => $products,
+            "message" => "Succesfully retrieved",
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->save();
+        return response()->json([
+            "data" => $product,
+            "message" => "Succesfully created!"
+
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::all()->find($id);
+
+        return response()->json([
+            "data" => $product,
+            "message" => "Succesfully retrieved"
+
+        ], 200);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::all()->find($id);
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->save();
+        return response()->json([
+            "data" => $product,
+            "message" => "Succesfully updated"
+
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::all()->find($id);
+        $product->delete();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/products', [\App\Http\Controllers\ProductsController::class, 'index']);
+Route::get('/products/{id}',[\App\Http\Controllers\ProductsController::class, 'show']);
+Route::post('/products',[\App\Http\Controllers\ProductsController::class, 'store']);
+Route::put('/products/{id}',[\App\Http\Controllers\ProductsController::class, 'update']);
+Route::delete('/products/{id}',[\App\Http\Controllers\ProductsController::class, 'destroy']);
